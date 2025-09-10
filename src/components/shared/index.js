3854ca830454e181e2a16561b473a1c559e07751
mritunjay.agarwal <mritunjay.agarwal@springworks.in>
2025-09-11 03:34:54 +0530

    Updates

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './Footer';
 export { default as PartnersSection } from './PartnersSection';
 export { default as SupportSection } from './SupportSection';
 export { default as CampaignSection } from './CampaignSection';
+export { default as WhatsAppButton } from './WhatsAppButton';
